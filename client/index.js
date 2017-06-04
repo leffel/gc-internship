ac64d242739fd792247e6597f743e7400a05f801
nicole🐊 <nicole.leffel@gmail.com>
2017-06-03 22:32:58 -0400

    fetch call data 📱

--- client/index.js
+++ client/index.js
@@ -9,19 +9,35 @@ var postAuthOptions = {
 }
 
 var loginCreds = JSON.stringify({ user: { email: 'user@example.com', password: 'doggo' } })
-
 var authRequest = http.request(postAuthOptions, function (res) {
   console.log('statusCode: ', res.statusCode)
   res.setEncoding('utf8')
   res.on('data', function (body) {
-    console.log(body)
+    var token = JSON.parse(body).token
+    renderCalls(token)
   })
 })
-
 authRequest.on('error', function (e) {
   console.error(e)
 })
-
 authRequest.write(loginCreds)
-
 authRequest.end()
+
+var renderCalls = function (token) {
+  var getCallsOptions = {
+    host: 'localhost',
+    port: 3000,
+    path: '/calls',
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-TOKEN': token }
+  }
+  var getCallData = http.request(getCallsOptions, function (res) {
+    res.setEncoding('utf8')
+    res.on('data', function (body) {
+      console.log(body)
+    })
+  })
+  getCallData.end()
+}
