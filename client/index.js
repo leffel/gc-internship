8d2e30eecf4d7b1cb94c9bf616cea348ee1d12be
nicole🐊 <nicole.leffel@gmail.com>
2017-06-03 23:07:00 -0400

    dynamic login creds, invalid login error msg

--- client/index.js
+++ client/index.js
@@ -1,27 +1,32 @@
 var http = require('http')
+var $ = require('jQuery')
 
-var postAuthOptions = {
-  host: 'localhost',
-  port: 3000,
-  path: '/authenticate',
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' }
-}
-
-var loginCreds = JSON.stringify({ user: { email: 'user@example.com', password: 'doggo' } })
-var authRequest = http.request(postAuthOptions, function (res) {
-  console.log('statusCode: ', res.statusCode)
-  res.setEncoding('utf8')
-  res.on('data', function (body) {
-    var token = JSON.parse(body).token
-    renderCalls(token)
+$('#login-submit').click(function () {
+  $('#login-errors').hide()
+  var postAuthOptions = {
+    host: 'localhost',
+    port: 3000,
+    path: '/authenticate',
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' }
+  }
+  var authRequest = http.request(postAuthOptions, function (res) {
+    res.setEncoding('utf8')
+    res.on('data', function (body) {
+      var token = JSON.parse(body).token
+      if (token != null) {
+        renderCalls(token)
+      } else {
+        $('#login-errors').show()
+      }
+    })
   })
+  var loginEmail = $('#login-email').val()
+  var loginPassword = $('#login-password').val()
+  var loginCreds = JSON.stringify({ user: { email: loginEmail, password: loginPassword } })
+  authRequest.write(loginCreds)
+  authRequest.end()
 })
-authRequest.on('error', function (e) {
-  console.error(e)
-})
-authRequest.write(loginCreds)
-authRequest.end()
 
 var renderCalls = function (token) {
   var getCallsOptions = {
