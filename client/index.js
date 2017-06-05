808bdf77af3f02165d947acbb21d8c6076280147
nicole🐊 <nicole.leffel@gmail.com>
2017-06-04 23:17:53 -0400

    some tidying up 🦄✨

--- client/index.js
+++ client/index.js
@@ -1,12 +1,16 @@
 var http = require('http')
 var $ = require('jQuery')
 
-$('#login-submit').click(function () {
+$('#login-submit').click(() => {
   $('#login-errors').hide()
+  var loginEmail = $('#login-email').val()
+  var loginPassword = $('#login-password').val()
+  var loginCreds = JSON.stringify({ user: { email: loginEmail, password: loginPassword } })
+
   var postAuthOptions = { host: 'localhost', port: 3000, path: '/authenticate', method: 'POST', headers: { 'Content-Type': 'application/json' } }
-  var authRequest = http.request(postAuthOptions, function (res) {
+  var authRequest = http.request(postAuthOptions, (res) => {
     res.setEncoding('utf8')
-    res.on('data', function (body) {
+    res.on('data', (body) => {
       var token = JSON.parse(body).token
       if (token != null) {
         renderCalls(token)
@@ -16,18 +20,15 @@ $('#login-submit').click(function () {
       }
     })
   })
-  var loginEmail = $('#login-email').val()
-  var loginPassword = $('#login-password').val()
-  var loginCreds = JSON.stringify({ user: { email: loginEmail, password: loginPassword } })
   authRequest.write(loginCreds)
   authRequest.end()
 })
 
 var renderCalls = function (token) {
   var getCallsOptions = { host: 'localhost', port: 3000, path: '/calls', method: 'GET', headers: { 'Content-Type': 'application/json', 'X-TOKEN': token } }
-  var getCallData = http.request(getCallsOptions, function (res) {
+  var getCallData = http.request(getCallsOptions, (res) => {
     res.setEncoding('utf8')
-    res.on('data', function (body) {
+    res.on('data', (body) => {
       var callData = JSON.parse(body)
       fillCallTable(callData)
     })
@@ -36,11 +37,11 @@ var renderCalls = function (token) {
 
   var fillCallTable = function (callData) {
     callData.calls.forEach((call) => {
-      var rowHTML = '<tr>' +
-                    '<td>' + call.sid + '</td>' +
-                    '<td>' + call.from + '</td>' +
-                    '<td>' + call.result + '</td>' +
-                    '</tr>'
+      var rowHTML = `<tr>
+                      <td> ${call.sid} </td>
+                      <td> ${call.from} </td>
+                      <td> ${call.result} </td>
+                    </tr>`
       $('#call-table').append(rowHTML)
     })
   }
